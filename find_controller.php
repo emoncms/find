e9ecbbecc0390c0c01dfcc22a1ba22f44d31be87
Trystan Lea <trystan.lea@gmail.com>
2019-01-08 19:19:24 +0000

    remove read access requirement

--- find_controller.php
+++ find_controller.php
@@ -11,20 +11,15 @@ function find_controller() {
     $result = false;
     
     // Read access API's and pages
-    if ($session['read']) {
-        if ($route->action == "") {
-            $route->format = 'html';
-            return view("Modules/find/Views/intro.php", array());
-        }
-        if ($route->action == "ok") {
-            $route->format = 'html';
-            return view("Modules/find/Views/find.php", array());
-        }
+    if ($route->action == "") {
+        $route->format = 'html';
+        return view("Modules/find/Views/intro.php", array());
+    }
+    if ($route->action == "ok") {
+        $route->format = 'html';
+        return view("Modules/find/Views/find.php", array());
     }
     
-    if ($session['write']) {
-        // unused
-    }
     // Pass back result
     return array('content' => $result);
 }
